d2cdfce483cebc17156f06ee6b45bdfb83028b68
fer-fleitas <144950222+fer-fleitas@users.noreply.github.com>
2026-09-25 17:18:41 -0300

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  poweredByHeader: false,
-  images: { formats: ["image/avif", "image/webp"] },
-};
-
-export default nextConfig;
